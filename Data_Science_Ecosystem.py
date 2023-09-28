852f0345e03ab1439c727a3bac59b23fb53b6274
lewisdecarolis <79420799+lewisdecarolis@users.noreply.github.com>
2023-09-27 20:16:19 -0500

    Add files via upload

---
+++ Data_Science_Ecosystem.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem 
+# 
+
+# Data Science Tools and Ecosystems are summarized in this notebook
+
+# **Objectives:**
+# 
+# - List Popular Languages for Data Science
+# - List Common Open Source Development Environments used by Data Scientists
+# - List Popular Librarires used in Data Science
+# - Introduce simple multiplication and addition expressions
+# - Introduce simple division/conversion expressions
+# 
+# 
+
+# ## Author
+# Lewis DeCarolis
+# 
+
+# Some of the popular tools Data Scientists use include:
+# 1. Python
+# 2. R
+# 3. SQL
+
+# Some of the commonly used libraries by Data Scientists include:
+# 1. Pandas
+# 2. Matplotlib
+# 3. Caret
+# 
+
+# |Data Science Tools|
+# ------------------------
+# |Jupyter|
+# |R Studio|
+# |Spyder|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python:
+# 
+
+# In[2]:
+
+
+#This a simple arithmetic expression to mutiply then add integers:
+
+(3 * 4) + 5
+
+
+# In[3]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+
+200 / 60
+
+
+# In[ ]:
+
+
+
+
